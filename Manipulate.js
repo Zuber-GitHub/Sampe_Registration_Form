516b2217d55ef875e283db122e617a8e31df30de
Zuber-GitHub <106949668+Zuber-GitHub@users.noreply.github.com>
2022-06-06 16:14:21 +0530

    list items modified

--- Manipulate.js
+++ Manipulate.js
@@ -1,10 +1,12 @@
-console.log(document.forms);
-const title = document.getElementById('header-title').innerHTML = 'Title is Changed'
-document.getElementById('header-title').style.color = 'yellow'
-document.getElementById('main-header').style.border = 'solid 5px black'
-
-document.getElementsByClassName('title')[0].innerHTML = '<b>ADD ITEM</b>'
-document.getElementsByClassName('title')[0].style.color = 'green'
-
-
-console.log("Header title is now: " + title);
+document.getElementById("header-title").innerText = "Changed Title"
+document.getElementById("main-header").style.border = "solid 5px black"
+console.log(document.getElementsByClassName("title")[0].innerText = "Add Items Changed")
+console.log(document.getElementsByClassName("title")[0].style.color = "green")
+console.log(document.getElementsByClassName("title")[0].style.fontWeight = "bold")
+console.log(document.getElementsByClassName("title")[0].style.fontWeight = "bold")
+console.log(document.getElementsByClassName("title")[0].style.fontWeight = "bold")
+const list = document.getElementsByClassName("list-group-item")
+console.log(list[2].style.backgroundColor = 'green')
+for(i of list){
+    i.style.fontWeight = 'bold'
+}
