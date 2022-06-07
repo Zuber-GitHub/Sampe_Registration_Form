f74d508a8b1ffebec853a604fbe538b8f65f4a8a
Zuber-GitHub <106949668+Zuber-GitHub@users.noreply.github.com>
2022-06-07 14:13:11 +0530

    Edit and Delete Button

--- Manipulate.js
+++ Manipulate.js
@@ -1,53 +1,58 @@
-// document.getElementById("header-title").innerText = "Changed Title"
-// document.getElementById("main-header").style.border = "solid 5px black"
-// console.log(document.getElementsByClassName("title")[0].innerText = "Add Items Changed")
-// console.log(document.getElementsByClassName("title")[0].style.color = "green")
-// console.log(document.getElementsByClassName("title")[0].style.fontWeight = "bold")
-// console.log(document.getElementsByClassName("title")[0].style.fontWeight = "bold")
-// console.log(document.getElementsByClassName("title")[0].style.fontWeight = "bold")
-// const list = document.getElementsByClassName("list-group-item")
-// console.log(list[2].style.backgroundColor = 'green')
-// for(i of list){
-//     i.style.fontWeight = 'bold'
-// }
-// console.log(document.getElementsByClassName('list-group')[0].children[4].textContent = "Modified by Class Name")
-// console.log(document.getElementsByTagName('li')[4].textContent = 'Modified by Tagname')
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
 
 
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
 
 
-// console.log(document.querySelector('.list-group').childNodes[5].style.backgroundColor = 'red')
-// console.log(document.querySelector('.list-group').childNodes[5].style.display = 'None')
+// Add item
+function addItem(e){
+  e.preventDefault();
 
-// let item  = document.querySelectorAll(".list-group")
-// console.log(item[0].children[1].style.backgroundColor = 'green')
+  // Get input value
+  var newItem = document.getElementById('item').value;
 
-// let item  = document.querySelectorAll(".list-group")[0].children
-// console.log(item.length)
-// for(let i=0;i<item.length;i=i+2){
-//     item[i].style.backgroundColor = 'green'
-// }
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
 
-const newDiv = document.createElement('div')
-newDiv.className = 'items'
-newDiv.id = 'item'
-let newDivText = document.createTextNode("Hello World")
-newDiv.appendChild(newDivText)
-newDiv.setAttribute('title','Text Appended before header')
-let container = document.querySelector('.container')
-console.log(container)
-let h1 = document.querySelector('header h1')
-container.insertBefore(newDiv,h1)
+  // Create del button element
+ 
+  var deleteBtn = document.createElement('button');
+  var editBtn = document.createElement('button')
 
-let firstDiv  = document.createElement('div')
+  // Add classes to del button
+  
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  editBtn.className = 'btn btn-sm float-right edit'
 
-firstDiv.id = 'firstDivId'
+  // Append text node
+  
+  deleteBtn.appendChild(document.createTextNode('X'));
+  editBtn.appendChild(document.createTextNode("Edit"))
 
-let firstDivDicText = document.createTextNode('Text before items')
-firstDiv.appendChild(firstDivDicText)
+  // Append button to li
+  
+  li.appendChild(deleteBtn);
+  li.append(editBtn)
 
-let temp = (document.querySelector('#items'))
+  // Append li to list
+  itemList.appendChild(li);
+}       
 
-let container_01 = document.querySelector("#main")
-console.log(container_01)
-container_01.insertBefore(firstDiv,temp)
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
