8730d4fa60fd28c88143a5b915837dad77d65c33
Zuber-GitHub <106949668+Zuber-GitHub@users.noreply.github.com>
2022-06-08 17:02:55 +0530

    Displaying Registered Students

--- registrationForm.js
+++ registrationForm.js
@@ -1,20 +1,35 @@
 let formName = document.getElementsByTagName("form")[0]
 let display = formName.addEventListener('submit',sendData)
+let count_01 = 0
 
 function sendData(e)
 
 
 {   
+    count_01+=1
     e.preventDefault()
-    let infoObj = {
+    let infoObj= {
     "Name": document.getElementById("fname").value,
     "Email": document.getElementById("email").value,
     "Phone": document.getElementById("phone").value,
-    "Time":  document.getElementById("birthdaytime").value
+    "Time":  document.getElementById("birthdaytime").value,
+    "count_01":count_01
     }
     
     let serializedObj = JSON.stringify(infoObj)
-    localStorage.setItem("Info",serializedObj)
+    let newName = localStorage.setItem(infoObj.count_01,serializedObj)
+    let reloadObj = localStorage.getItem(infoObj.count_01)
+    let reloadString = JSON.parse(reloadObj)
+    let li = document.createElement('li')
+    li.className = 'listItems'
+    li.appendChild(document.createTextNode(reloadString.Name + " " + 'Email is:' +  reloadString.Email))
+    let mainClass = document.querySelector('#users')
+    mainClass.appendChild(li)
+
+    
+    
+    
+    
     
     
 //     localStorage.setItem('The Name is',document.getElementById("fname").value)
@@ -24,9 +39,9 @@ function sendData(e)
 // 
     
 }
-let reloadObj = localStorage.getItem("Info")
-let reloadString = JSON.parse(reloadObj)
-document.getElementById("fname").value = reloadString.Name
-document.getElementById("email").value = reloadString.Email
-document.getElementById("phone").value = reloadString.Phone
-document.getElementById("birthdaytime").value = reloadString.Time
+// let reloadObj = localStorage.getItem("Info")
+// let reloadString = JSON.parse(reloadObj)
+// document.getElementById("fname").value = reloadString.Name
+// document.getElementById("email").value = reloadString.Email
+// document.getElementById("phone").value = reloadString.Phone
+// document.getElementById("birthdaytime").value = reloadString.Time
