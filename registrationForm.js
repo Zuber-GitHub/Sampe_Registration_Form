d24f938e2ba41910aa55662ba459a4039191521c
Zuber-GitHub <106949668+Zuber-GitHub@users.noreply.github.com>
2022-06-08 12:31:15 +0530

    Adding Information as Object in Local Storage

--- registrationForm.js
+++ registrationForm.js
@@ -2,9 +2,23 @@ let formName = document.getElementsByTagName("form")[0]
 formName.addEventListener('submit',sendData)
 
 function sendData(e)
-{
-    localStorage.setItem('The Name is',document.getElementById("fname").value)
-    localStorage.setItem('The Email is',document.getElementById("email").value)
-    localStorage.setItem('The Phone Number is',document.getElementById("phone").value)
-    localStorage.setItem('The Meeting time is',document.getElementById("birthdaytime").value)
+
+
+{   e.preventDefault()
+    let infoObj = {
+    "Name": document.getElementById("fname").value,
+    "Email": document.getElementById("email").value,
+    "Phone": document.getElementById("phone").value,
+    "Time":  document.getElementById("birthdaytime").value
+    }
+    
+    let serializedObj = JSON.stringify(infoObj)
+    localStorage.setItem("Info",serializedObj)
+    
+    
+//     localStorage.setItem('The Name is',document.getElementById("fname").value)
+//     localStorage.setItem('The Email is',document.getElementById("email").value)
+//     localStorage.setItem('The Phone Number is',document.getElementById("phone").value)
+//     localStorage.setItem('The Meeting time is',document.getElementById("birthdaytime").value)
+// 
 }
